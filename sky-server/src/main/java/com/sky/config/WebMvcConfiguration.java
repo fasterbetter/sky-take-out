ed4cbe20c5fb098cf21c9a7b2a70d4b43ebb1759
fasterbetter <malloctime@163.com>
2024-03-27 12:25:32 +0800

    员工分页查询业务代码

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,14 +1,10 @@
 package com.sky.config;
 
-import com.sky.entity.Employee;
 import com.sky.interceptor.JwtTokenAdminInterceptor;
-import com.sky.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -18,8 +14,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.List;
 
 /**
  * 配置类，注册web层相关组件
@@ -41,20 +35,6 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/employee/login");
-    }
-
-    /**
-     * 扩展Spring MVC框架的消息转换器
-     * @param converters
-     */
-    @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        //创建一个消息转换器对象
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        //为消息转换器设置一个对象转换器，对象转换器可以将Java对象序列化为Json数据
-        converter.setObjectMapper(new JacksonObjectMapper());
-        //将自定义的消息转换器添加到容器中,0表示设置该转换器位于容器第一位
-        converters.add(0, converter);
     }
 
     /**
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,7 +1,5 @@
 package com.sky.mapper;
 
-import com.github.pagehelper.Page;
-import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -26,11 +24,4 @@ public interface EmployeeMapper {
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
             " values (#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void save(Employee employee);
-
-    /**
-     * 分页查询
-     * @param employeePageQueryDTO
-     * @return
-     */
-    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
